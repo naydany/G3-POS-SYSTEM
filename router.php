f68291edee4ba31e0a16852bdc684aa17b27428a
CHANNICH.NOEURN <CHANNICH.NOEURN@PNC.LAN>
2024-02-19 10:42:19 +0700

    [imp] update style of table products POS-29

--- router.php
+++ router.php
@@ -8,11 +8,10 @@ $routes = [
     '/orders' => 'controllers/orders/order.controller.php',
     '/reports' => 'controllers/reports/report.controller.php',
     '/users' => 'controllers/users/user.controller.php',
-    '/create_form_cate' => 'controllers/categories/form_category.controller.php',
-    '/payments' => 'controllers/payments/payment.controller.php',
-    '/staffs' => 'controllers/staffs/staff.controller.php',
-
-
+    '/form_create' => 'controllers/items/create_item.controller.php',
+    '/edit_item' => 'controllers/items/edit_item.controller.php',
+ 
+    
 ];
 
 if (array_key_exists($uri, $routes)) {
