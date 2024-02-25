854a95447da2e433a013a3d2e7b053469a80397a
Socheathek Mao <socheatek.mao@student.passerellesnumeriques.org>
2024-02-25 12:00:20 +0700

    [feat] show_password POS-74

--- views/staffsignin/staff.signin.view.php
+++ views/staffsignin/staff.signin.view.php
@@ -42,19 +42,43 @@ endif;
                                 <div class="form-group mb-3">
                                     <div class="input-group input-group-alternative">
                                         <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="bi bi-envelope-fill"></i></span>
+                                            <span class="input-group-text"><i class="bi bi-envelope"></i></span>
                                         </div>
                                         <input class="form-control" required name="staff_email" placeholder="Email" type="email">
                                     </div>
                                 </div>
-                                <div class="form-group">
-                                    <div class="input-group input-group-alternative">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text"><i class="bi bi-unlock-fill"></i></span>
-                                        </div>
-                                        <input class="form-control" required name="staff_password" placeholder="Password" type="password">
+
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="bi bi-shield-lock"></i></span>
                                     </div>
+                                    <input type="password" class="form-control" id="passwordInput" required name="staff_password" placeholder="Password">
+                                    <div class="input-group-append">
+                                        <span class="input-group-text" style="display: block;" id="hidePass" onclick="togglePasswordVisibility()"><i class="bi bi-eye-slash"></i></span>
+                                        <span class="input-group-text" style="display: none;" id="showPass" onclick="togglePasswordVisibility()"><i class="bi-eye"></i></span>
+                                    </div>
+
+                                    <script>
+                                        const passwordInput = document.getElementById("passwordInput");
+                                        const hidePass = document.getElementById("hidePass");
+                                        const showPass = document.getElementById("showPass");
+
+                                        function togglePasswordVisibility() {
+                                            if (passwordInput.type === "password") {
+                                                passwordInput.type = "text";
+                                                hidePass.style.display = "none";
+                                                showPass.style.display = "block";
+                                            } else {
+                                                passwordInput.type = "password";
+                                                hidePass.style.display = "block";
+                                                showPass.style.display = "none";
+                                            }
+                                        }
+                                        togglePasswordIcon.addEventListener("click", togglePasswordVisibility);
+                                    </script>
                                 </div>
+
+
                                 <div class="custom-control custom-control-alternative custom-checkbox">
                                     <input class="custom-control-input" id=" customCheckLogin" type="checkbox">
                                     <label class="custom-control-label" for=" customCheckLogin">
@@ -62,13 +86,10 @@ endif;
                                     </label>
                                 </div>
                                 <div class="row justify-content-between">
-                                    <a href="/" class="btn btn-primary my-4">
-                                        < Back</a>
-                                            <a href="/admin" class="btn btn-primary my-4">Log In</a>
+                                    <a href="/" class="btn btn-primary my-4"><i class="bi bi-chevron-double-left"> Back</i></a>
+                                    <a href="/admin" class="btn btn-primary my-4">Log In</a>
                                 </div>
-
                             </form>
-
                         </div>
                     </div>
 
