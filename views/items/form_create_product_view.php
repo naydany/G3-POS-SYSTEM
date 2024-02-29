a8d93197074f3174a8f1d0fa37d65c09e5320af7
SOKHEANG.BOTH <SOKHEANG.BOTH@pnc.lan>
2024-02-29 20:57:49 +0700

    [imp] to update search order

--- views/items/form_create_product_view.php
+++ views/items/form_create_product_view.php
@@ -16,6 +16,18 @@
                 <label>Image</label>
                 <input type="file" class="form-control" placeholder="Insert Image" name="image">
             </div>
+            <div class="form-group">
+                <label>supplier</label>
+                <!-- <input type="text" class="form-control" placeholder="Enter Category" name="cate"> -->
+                <div class="input-group mb-3">
+                    <select class="custom-select" id="inputGroupSelect01" name="supplier">
+                        <option selected>Choose supplier...</option>
+                        <?php for($i = 0; $i<count($suppliers);$i++) : ?>
+                              <option value="<?= $suppliers[$i][0] ?>"><?= $suppliers[$i][0] ?></option>  
+                        <?php endfor; ?>
+                    </select>
+                </div>
+            </div>
             <button type="submit" class="btn btn-primary w-25">Submit</button>
 
         </div>
