0ee98406ae1af9d5d39c2175280ad2f2c5a10a4b
SOKHEANG.BOTH <SOKHEANG.BOTH@PNC.LAN>
2024-03-12 20:50:47 +0700

    [feat] to_order_and_to_payment PSG-28

--- controllers/payments/old_payment.controller.php
+++ controllers/payments/old_payment.controller.php
@@ -1,6 +1,7 @@
 <?php
 require "../../database/database.php";
 require "../../models/payment.model.php";
+require "../../models/order.model.php";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     if ($_POST['customerID'] !== '' && $_POST['code'] !== '' && $_POST['amounts'] !== '' && $_POST['pay_method'] !== '') {
@@ -8,16 +9,19 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $amounts = $_POST['amounts'];
         $customerID = $_POST['customerID'];
         $code = $_POST['code'];
+        $Paid = "Paid";
 
         $news = getPayments();
         foreach ($news as $new) {
             $date = $new['pay_date'];
             $addMore_oldPayment = addMoreoldPayment($method, $customerID, $date);
+            $Paid = updateOrder($Paid,$date);
         }
     }
 }
 $add_oldPayment = oldPayment();
 $getOldPayments = getOldPayments();
+
 deletOrder();
 header('Location:/payments');
 
--- views/payments/payment.view.php
+++ views/payments/payment.view.php
@@ -6,18 +6,8 @@
                 Make A New Order
             </a>
         </div>
-        <div class="d-flex">
-            <div class="card-header border-0">
-                <a href="/old_payment" class="btn btn-outline-primary">
-                    <i class="fas fa-plus"></i> <i class="bi bi-currency-dollar"></i>
-                    Start to Pay
-                </a>
-            </div>
-        </div>
-    </div>
-    </div>
-    </div>
 
+    </div>
     <div class="container mt-3">
         <table class="table table-bordered text-center mt-2 rounded">
             <thead class="text-secondary thead-light">
@@ -64,8 +54,15 @@
         $ToTal +=  $new['pay_totalprice'];
     }
     ?>
-    <h3 class="bg-success border border-primary rounded p-1 m-4 text-white">Total : <?php echo $ToTal ?>$</h3>
-    <?php require "views/payments/old_payment.view.php";?>
+    <h3 class="bg-success border border-primary rounded p-1 pl-4 m-4 text-white">Total : <?php echo $ToTal ?>$</h3>
+    <div class="d-flex justify-content-end">
+        <div class="align-self-end ">
+            <a href="/old_payment" class="btn btn-outline-primary mr-4">
+                <i class="fas fa-plus"></i> <i class="bi bi-currency-dollar"></i>
+                Start to Pay
+            </a>
+        </div>
+    </div>
 </div>
-
-
+<?php require "views/payments/old_payment.view.php"; ?>
+</div>
