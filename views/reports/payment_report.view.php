f4e6cabed8c55ffa6e3a6589b9dcae7b8ba9511a
CHANNICH NOEURN <annenichz1507>
2024-03-15 09:05:26 +0700

    update_payment

--- views/reports/payment_report.view.php
+++ views/reports/payment_report.view.php
@@ -3,7 +3,6 @@
         <table class="table table-bordered text-center mt-2 rounded">
             <h4 class="m-5 text-primary">Payment Report</h4>
             <thead class="text-secondary thead-light">
-
                 <tr>
                     <th>Payment Code</th>
                     <th>Payment Method</th>
@@ -33,5 +32,4 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
-
 </html>
