7eea252b067ccb5089a8045a86497f31c0c30e25
CHANNICH NOEURN <annenichz1507>
2024-03-18 08:48:46 +0700

    [imp] update_link_javaScript_of_export_sale_report_to_excel PSG-37

--- views/reports/sale_report.view.php
+++ views/reports/sale_report.view.php
@@ -1,10 +1,9 @@
-
-    <div class="card-header py-3 d-flex justify-content-between">
-        <h4 class="m-5 text-primary">Sales Reports</h4>
-        <button class="btn btn-outline-primary h-50 mt-5" id="export_button"><i class="bi bi-file-earmark-arrow-down"></i>
-            Export Data
-        </button>
-    </div>
+<div class="card-header py-3 d-flex justify-content-between">
+    <h4 class="m-5 text-primary">Sales Reports</h4>
+    <button class="btn btn-outline-primary h-50 mt-5" id="export_button"><i class="bi bi-file-earmark-arrow-down"></i>
+        Export Data
+    </button>
+</div>
 </div>
 <div class="container mt-3">
     <table class="table table-bordered text-center" id="sale_data">
@@ -41,15 +40,21 @@
             <?php endforeach; ?>
         </tbody>
     </table>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
-<script>
-    function export_table_data_to_excel(type) {
-        var data = document.getElementById("sale_data");
-        var file = XLSX.utils.table_to_book(data, {sheet: "sheet1"});
-        XLSX.writeFile(file, 'file.' + type);
-    }
-    const export_button = document.getElementById("export_button");
-    export_button.addEventListener('click', () => {
-        export_table_data_to_excel('xlsx');
-    });
-</script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script>
+        function export_table_data_to_excel(type) {
+            var data = document.getElementById("sale_data");
+            var file = XLSX.utils.table_to_book(data, {
+                sheet: "sheet1"
+            });
+            XLSX.writeFile(file, 'file.' + type);
+        }
+        const export_button = document.getElementById("export_button");
+        export_button.addEventListener('click', () => {
+            export_table_data_to_excel('xlsx');
+        });
+    </script>
