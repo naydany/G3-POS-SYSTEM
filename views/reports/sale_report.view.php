a2178ccbd9b1e01785e6db9bfb308d54615fdd9a
CHANNICH NOEURN <annenichz1507>
2024-03-18 08:46:29 +0700

    [imp] update_employee_report_to_excel PSG-37

--- views/reports/sale_report.view.php
+++ views/reports/sale_report.view.php
@@ -4,45 +4,41 @@
         Export Data
     </button>
 </div>
+<table class="table table-bordered text-center mt-2 rounded">
+    <thead class="text-secondary thead-light">
 
-<div class="container mt-3">
-    <h4 class="text-primary" style="margin-top: 5%;">Orders Reports</h4>
-    <table class="table table-bordered text-center mt-2 rounded">
-        <thead class="text-secondary thead-light">
-
+        <tr>
+            <th>Order ID</th>
+            <th>Product</th>
+            <th>Unit Price</th>
+            <th>Total Price</th>
+            <th>Status</th>
+            <th>Date</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        require "models/order.model.php";
+        $orders = getOrdersDetail();
+        foreach ($orders as $order) : ?>
             <tr>
-                <th>Order ID</th>
-                <th>Product</th>
-                <th>Unit Price</th>
-                <th>Total Price</th>
-                <th>Status</th>
-                <th>Date</th>
+                <td><?= $order['order_detail_id'] ?></td>
+                <td><?= $order['pro_name'] ?></td>
+                <td><?= $order['pro_price'] ?></td>
+                <td><?= $order['tatal_price'] ?>$</td>
+                <td>
+                    <?php if ($order['order_status'] != "Paid" && $order['order_status'] != 1) {
+                        echo "<span class='badge badge-danger'>Not Paid</span>";
+                    } else {
+                        echo "<span class='badge badge-success'>Paid</span>";
+                    }
+                    ?>
+                </td>
+                <td><?= $order['order_date'] ?></td>
             </tr>
-        </thead>
-        <tbody>
-            <?php
-            require "models/order.model.php";
-            $orders = getOrdersDetail();
-            foreach ($orders as $order) : ?>
-                <tr>
-                    <td><?= $order['order_detail_id'] ?></td>
-                    <td><?= $order['pro_name'] ?></td>
-                    <td><?= $order['pro_price'] ?></td>
-                    <td><?= $order['tatal_price'] ?>$</td>
-                    <td>
-                        <?php if ($order['order_status'] != "Paid" && $order['order_status'] != 1) {
-                            echo "<span class='badge badge-danger'>Not Paid</span>";
-                        } else {
-                            echo "<span class='badge badge-success'>Paid</span>";
-                        }
-                        ?>
-                    </td>
-                    <td><?= $order['order_date'] ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</div>
+        <?php endforeach; ?>
+    </tbody>
+</table>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.17.5/xlsx.full.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
