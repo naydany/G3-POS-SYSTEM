dbd4627ae662b52318ea3c6bbba5ba0483e59e26
CHANNICH NOEURN <annenichz1507>
2024-03-19 20:35:46 +0700

    [fix_bug] fix_conflict_code_of_category

--- controllers/categories/create_category.controller.php
+++ controllers/categories/create_category.controller.php
@@ -7,9 +7,20 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $category = $_POST["category"];
         $description = $_POST['description'];
 
+<<<<<<< HEAD
         $add = createCategory($category, $description);
             header("Location:/categories");
     }else {
         echo "Please file all form";
+=======
+
+    $add = createCategory($category);
+    echo $add;
+
+    if($add){
+        header("Location:/categories");
+    }else{
+        header("Location:/create_form_cate");
+>>>>>>> fc55b72 ([imp] update style of table in category POS-24)
     }
 }
