e91e91940fb643fc26269c78ffea0473cd8d60cb
CHANNICH NOEURN <annenichz1507>
2024-03-19 20:41:14 +0700

    [fix_bug] to_make_create_category_secure POS-27

--- controllers/categories/create_category.controller.php
+++ controllers/categories/create_category.controller.php
@@ -4,8 +4,8 @@ require "../../models/category.model.php";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     if ($_POST['category'] !== '' && $_POST['description'] !== '' ) {
-        $category = $_POST["category"];
-        $description = $_POST['description'];
+        $category = htmlspecialchars($_POST["category"]);
+        $description = htmlspecialchars($_POST['description']);
 
         $add = createCategory($category, $description);
             header("Location:/categories");
