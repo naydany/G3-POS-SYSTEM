018e3e64605aeef0f395b9299d2867afac5d8dc6
CHANNICH NOEURN <annenichz1507>
2024-03-20 09:05:19 +0700

    [imp] improve_code_more_secure_in_update_staff POS-105

--- controllers/staffs/update_staff.controller.php
+++ controllers/staffs/update_staff.controller.php
@@ -4,11 +4,11 @@ require '../../models/staff.model.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
  
     if ($_POST['cas_id'] !== '' && $_POST['name'] !=='' && $_POST['number'] !=='' && $_POST['email'] !=='' && $_POST['address'] !=='') {
-        $name = $_POST['name'];
-        $number = $_POST['number'];
-        $email = $_POST['email'];
-        $address = $_POST['address'];
-        $id = $_POST['cas_id'];
+        $name = htmlspecialchars($_POST['name']);
+        $number = htmlspecialchars($_POST['number']);
+        $email = htmlspecialchars($_POST['email']);
+        $address = htmlspecialchars($_POST['address']);
+        $id = htmlspecialchars($_POST['cas_id']);
         
         $update = updateStaffs($id, $name, $number, $email, $address);
 
